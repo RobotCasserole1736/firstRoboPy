c39bd3afe27857b4326348e14519d8d06d8afc1c
Chris Gerth <chrisgerth010592@gmail.com>
2023-04-15 07:24:23 -0500

    WIP

---
+++ Loggable/__init__.py
@@ -0,0 +1,11 @@
+def hasSignals(original_class):
+    orig_init = original_class.__init__
+    # Make copy of original __init__, so we can call it without recursion
+
+    def __init__(self, id, *args, **kws):
+        self.__id = id
+        self.getId = getId
+        orig_init(self, *args, **kws) # Call the original __init__
+
+    original_class.__init__ = __init__ # Set the class' __init__ to the new one
+    return original_class
