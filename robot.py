0a708b0d40f4810141e1e08c12704dc2078798cb
Chris Gerth <chrisgerth010592@gmail.com>
2023-04-27 08:21:50 -0500

    First pass signal logging

--- robot.py
+++ robot.py
@@ -1,16 +1,25 @@
 import wpilib
-
+import utils.signalLogging as sl
 
 class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
         print("It's Init Time")
     
-    def robotPeriodic(self):
-        print("Periodic")
+    def teleopInit(self):
+        pass
         
     def teleopPeriodic(self):
-        print("teleop")
+        sl.log("test", 42)
+    
+    def autonomousInit(self):
+        pass
+        
+    def autonomousPeriodic(self):
+        sl.log("test", 23)
+        
+    def robotPeriodic(self):
+        sl.publish()
         
         
 if __name__ == '__main__':
