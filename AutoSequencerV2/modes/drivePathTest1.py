4298df858e9e34970feed1fec583ee6e2364bfd9
Chris Gerth <chrisgerth010592@gmail.com>
2023-09-27 21:09:54 -0500

    oop found another bug

--- AutoSequencerV2/modes/drivePathTest1.py
+++ AutoSequencerV2/modes/drivePathTest1.py
@@ -2,16 +2,16 @@
 from AutoSequencerV2.commands.drivePathCommand import DrivePathCommand
 from AutoSequencerV2.mode import Mode
 
-# A DoNothingMode is an autonomous mode where the robot just sits doing nothing indefinitely
+# A DrivePathTest1 is an autonomous mode which drives the specific path designed in the deploy/pathplanner/testPath1.path file
 class DrivePathTest1(Mode):
     def __init__(self):
-        # Build a reasonable name out of the specified duration
         Mode.__init__(self, f"Drive Path Test 1")
         self.pathCmd = DrivePathCommand("testPath1", 1.0)
         
     def getCmdGroup(self):
-        # A wait mode should have only one command, jsut wait the specified duration
+        # Just return the path command
         return self.pathCmd
     
     def getInitialDrivetrainPose(self):
-        return self.pathCmd.path.getInitialPose()
+        # Use the path command to specify the starting pose
+        return self.pathCmd.path.getInitialHolonomicPose()
